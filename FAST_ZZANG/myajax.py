1cd56c90a03e0e28850d07a8eec7e3fb9db84ff5
anhy111 <gkdyd123@gmail.com>
2022-10-18 19:56:13 +0900

    python

--- FAST_ZZANG/myajax.py
+++ FAST_ZZANG/myajax.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, Form, WebSocket
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles 
 from fastapi.templating import Jinja2Templates
@@ -18,5 +18,15 @@ async def zzang(request: Request):
 @app.get('/zzang2', response_class=HTMLResponse)
 async def zzang2(request: Request):
     return templates.TemplateResponse("zzang2.html", {"request": request})
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    print(f"client connected : {websocket.client}")
+    await websocket.accept() # client의 websocket접속 허용
+    await websocket.send_text(f"Welcome client : {websocket.client}")
+    while True:
+        data = await websocket.receive_text()  # client 메시지 수신대기
+        print(f"message received : {data} from : {websocket.client}")
+        await websocket.send_text(f"Message text was: {data}") # client에 메시지 전달
 
 # uvicorn myajax:app --reload
